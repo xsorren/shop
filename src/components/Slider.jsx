378f09f3acc4e097ff2cfdaeca3391c81238874c
xsorren <lauta.sorren@gmail.com>
2022-07-05 20:33:10 -0300

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,7 +9,7 @@ const Container = styled.div`
   display: flex;
   position: relative;
   overflow: hidden;
-  ${mobile({ display: "none" })}
+  ${mobile({ display: "none"})}
 `;
 
 const Arrow = styled.div`
